42f6b44ac3eb08b0981a8ca6d1e61c570a021e73
farid <imakhfaruid@gmail.com>
2022-10-10 10:36:51 +0200

    key change

--- src/main/java/com/org/far/principals/KafkaAvroproducer.java
+++ src/main/java/com/org/far/principals/KafkaAvroproducer.java
@@ -30,7 +30,7 @@ public class KafkaAvroproducer {
                 Station station = geOneRecordtStation(getAllStations, i);
                 ProducerRecord<String, Station> producerRecord = new ProducerRecord<>(
 
-                        getTOPICSTATION(), station.getName().replaceAll("\\s","").trim().toLowerCase(), station);
+                        getTOPICSTATION(), station.getName().toLowerCase(), station);
                 producer.send(producerRecord, (metadata, exception) -> {
                     if (exception == null) {
                         System.out.println(producerRecord.key()+ " -----> " + producerRecord.value().getContractName());
